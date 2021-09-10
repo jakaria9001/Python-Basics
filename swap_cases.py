9e1d320f61ee9977ededb9b58737632381356df2
Hussain <jakariahussain9001@gmail.com>
2021-09-10 22:30:02 +0530

    Added program to swap cases of letter in python

---
+++ swap_cases.py
@@ -0,0 +1,17 @@
+# # Program to check prime number: 
+# def myfunc(n):
+#     if n < 2:
+#         return False
+#     for factor in range(2, n):
+#         if n % factor == 0:
+#             return False
+#     return True
+
+# print(myfunc(21))
+
+# # Program to swap cases of a character
+# class Solution:
+#     # @param A : string
+#     # @return a strings
+#     def solve(self, A):
+#         return A.swapcase()
